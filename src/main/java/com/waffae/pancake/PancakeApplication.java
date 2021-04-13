aa22cb2e74873828d143ba918b537b00096eda13
nick <15068796383@163.com>
2021-04-13 11:22:14 +0800

    [feat]- 1.jieba中文分词器测试。

--- src/main/java/com/waffae/pancake/PancakeApplication.java
+++ src/main/java/com/waffae/pancake/PancakeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * starter
  *
- * @ yixiaoshuang
+ * @yixiaoshuang
  */
 @SpringBootApplication
 public class PancakeApplication {

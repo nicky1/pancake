8c2f22adcdf31844cb492e341d5a7aa8e1017eb9
易晓双 <15068796383@163.com>
2023-04-13 23:20:39 +0800

    ttl

---
+++ src/main/java/com/waffle/pancake/integrated/tech/thread/ttl/Test.java
@@ -0,0 +1,12 @@
+package com.waffle.pancake.integrated.tech.thread.ttl;
+
+/**
+ * @desc:
+ * @author: yixiaoshuang
+ * @date: 2023/3/28
+ **/
+public class Test {
+    int test1() {
+        return 1;
+    }
+}
--- src/main/java/com/waffle/pancake/integrated/tech/thread/ttl/TestTTL.java
+++ src/main/java/com/waffle/pancake/integrated/tech/thread/ttl/TestTTL.java
@@ -3,7 +3,10 @@ package com.waffle.pancake.integrated.tech.thread.ttl;
 import com.alibaba.ttl.TransmittableThreadLocal;
 import com.alibaba.ttl.TtlRunnable;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @description: 测试阿里开源的多线程框架TTL，解决在线程池场景下,线程间传递变量串的问题。
@@ -12,12 +15,23 @@ import java.util.concurrent.*;
  */
 public class TestTTL {
 
+    int j = 0;
+
+    TransmittableThreadLocal<String> context = new TransmittableThreadLocal(){
+
+        @Override
+        public String copy(Object parentValue) {
+            return new String(String.valueOf(parentValue));
+        }
+    };
 
     /**
      * 测试父子线程，threadLocal 值传递
      */
     public void testParentThread() {
-        TransmittableThreadLocal<String> context = new TransmittableThreadLocal<>();
+        TransmittableThreadLocal<String> context = new TransmittableThreadLocal(){
+
+        };
         context.set("value parent");
 
         Thread thread = new Thread(() -> {
@@ -29,36 +43,38 @@ public class TestTTL {
 
 
     /**
-     * 待验证，线程池场景，怎么写测试用例
+     * 待验证，线程池场景，怎么写测试用例`
      */
     public void testThreadFactory() {
-        TransmittableThreadLocal<String> context = new TransmittableThreadLocal<>();
+//        InheritableThreadLocal context = new InheritableThreadLocal();
         context.set("value parent");
 
-        ExecutorService executorService = new ThreadPoolExecutor(10, 15, 1, TimeUnit.SECONDS, new ArrayBlockingQueue<>(5));
+        ExecutorService executorService = new ThreadPoolExecutor(2, 3, 1, TimeUnit.SECONDS, new ArrayBlockingQueue<>(5));
 
-        for (int i = 0; i < 20; i++) {
-            context.set("value parent");
+//        ExecutorService ttlExecutorService = TtlExecutors.getTtlExecutorService(executorService);
 
-            Runnable task = () -> System.out.println(context.get());
-            if (i < 1) {
-                context.set("s1");
+        for (int i = 0; i < 5; i++) {
 
-                TtlRunnable ttlRunnable = TtlRunnable.get(task);
-                executorService.submit(task);
+            Runnable task = () -> {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                }
+                System.out.println(context.get());
+            };
 
-            }else{
-                context.set("s2");
-                TtlRunnable ttlRunnable = TtlRunnable.get(task);
+            context.set("s2:"+(++j));
+            TtlRunnable ttlRunnable = TtlRunnable.get(task);
+            executorService.submit(ttlRunnable);
 
-                executorService.submit(task);
-            }
+            context.set("s2:"+(++j));
 
         }
+        System.out.println("end");
 
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         TestTTL test = new TestTTL();
 //        test.testParentThread();
 
